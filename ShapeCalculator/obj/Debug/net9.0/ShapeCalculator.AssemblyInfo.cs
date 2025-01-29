c98f7da7387221228a1611ae3fd43e20fcc746ce
Irving <irvingfsanchez@proton.me>
2025-01-29 12:35:18 -0600

    adjusting commits

--- ShapeCalculator/obj/Debug/net9.0/ShapeCalculator.AssemblyInfo.cs
+++ ShapeCalculator/obj/Debug/net9.0/ShapeCalculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShapeCalculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9049ee59262285bd57122ee5d794996014040721")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShapeCalculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShapeCalculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
